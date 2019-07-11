bd2e1ccffac669dda8e1d1bfb5b703c549970a84
zhanghang <1294122168@qq.com>
2019-07-11 22:26:39 +0800

    changeAuth

---
+++ note-service/auth-service/src/main/java/cn/zh/auth/service/AuthApplication.java
@@ -0,0 +1,16 @@
+package cn.zh.auth.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhanghang
+ */
+@SpringBootApplication
+public class AuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthApplication.class, args);
+    }
+
+}
