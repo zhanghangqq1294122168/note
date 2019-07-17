77d4508577f2fa27e1a2f032fc52577e0f1c9d9f
zhanghang <1294122168@qq.com>
2019-07-17 23:57:55 +0800

    updateAuth

--- note-gateway/src/main/java/cn/zh/note/gateway/NoteGatewayApplication.java
+++ note-gateway/src/main/java/cn/zh/note/gateway/NoteGatewayApplication.java
@@ -2,6 +2,7 @@ package cn.zh.note.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableOAuth2Sso
 public class NoteGatewayApplication {
 
     public static void main(String[] args) {
