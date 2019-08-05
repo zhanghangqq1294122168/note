29ff943d75d3d1ffd22229aeff9628573e83b62e
zhanghang <1294122168@qq.com>
2019-08-05 22:46:07 +0800

    changeAuth

--- note-gateway/src/main/java/cn/zh/note/gateway/NoteGatewayApplication.java
+++ note-gateway/src/main/java/cn/zh/note/gateway/NoteGatewayApplication.java
@@ -2,7 +2,6 @@ package cn.zh.note.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -10,7 +9,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EnableOAuth2Sso
+//@EnableOAuth2Sso
 public class NoteGatewayApplication {
 
     public static void main(String[] args) {
--- note-gateway/src/main/java/cn/zh/note/gateway/config/SecurityConfig.java
+++ note-gateway/src/main/java/cn/zh/note/gateway/config/SecurityConfig.java
@@ -1,20 +1,20 @@
-package cn.zh.note.gateway.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * @author zhanghang
- */
-@Configuration
-@EnableWebSecurity
-@Order(99)
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-    }
-}
+//package cn.zh.note.gateway.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * @author zhanghang
+// */
+//@Configuration
+//@EnableWebSecurity
+//@Order(99)
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+//    }
+//}
